fc4eed352eb3c011511a5789b2b59ee1a7d5deee
koh-suzuki <k.s-coward-0629@ezweb.ne.jp>
2020-04-18 03:28:17 +0000

    ポートフォリオ用クローン

---
+++ MiniTweetApp/app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user
+  before_action :limitation_correct_user, only:[:edit, :update, :destroy]
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user)
+    if @post.save
+      flash[:notice] = "新規投稿しました！"
+      redirect_to posts_index_url
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました！"
+      redirect_to posts_index_url
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    flash[:notice] = "投稿を削除しました。"
+    redirect_to posts_index_url
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+  end
+  
+  def limitation_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user_id == @current_user.id
+      flash[:notice]= "自分以外のユーザーの投稿は編集できません。"
+      redirect_to posts_index_url
+    end
+  end
+end
